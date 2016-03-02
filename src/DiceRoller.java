65b54a748301d80ff220f905f90c716b770d567c
Sam Mazur <mazur.sam@gmail.com>
2016-03-02 15:29:35 -0500

    adding DiceRoller

---
+++ src/DiceRoller.java
@@ -0,0 +1,13 @@
+
+import java.util.Random;
+
+public class DiceRoller {
+	public static void main(String[] args) {
+		int dienumber;
+		Random rnd = new Random();
+
+		dienumber = 1 + rnd.nextInt(6);
+
+		System.out.println("Your die roll was : " + dienumber);
+	}
+}
